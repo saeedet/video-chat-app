5683fc016b27b920cac1f770ae9d08c494b68f18
Saeed <saeed_et90@yahoo.com>
2021-09-22 12:42:58 +1000

    final design chencgaes

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -5,10 +5,9 @@ const VideoPlayer = () => {
   const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } =
     useContext(SocketContext);
   return (
-    <div className="flex space-x-4 justify-center w-full px-4">
+    <div className="flex space-x-4 justify-center w-full px-4 mt-6">
       {stream && (
         <div className=" p-3 rounded-md bg-green-400">
-          {/* <h3 className=" flex justify-center">{name || "User"}</h3> */}
           <video
             className="rounded-md"
             playsInline
@@ -16,12 +15,17 @@ const VideoPlayer = () => {
             autoPlay
             ref={myVideo}
           />
+          <h3 className=" flex justify-center text-white font-bold text-xl mt-2">
+            {name || "User"}
+          </h3>
         </div>
       )}
       {callAccepted && !callEnded && (
         <div className=" p-3 rounded-md bg-blue-400">
-          {/* <h3 className="w-full flex justify-center">{call.name || "User"}</h3> */}
           <video className="rounded-md" playsInline autoPlay ref={userVideo} />
+          <h3 className="flex justify-center text-white font-bold text-xl mt-2">
+            {call.name || "User"}
+          </h3>
         </div>
       )}
     </div>
